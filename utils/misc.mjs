30905d1eb08bed9fad10d7db70c3f4233e453dee
Steven Luscher <steven.luscher@anza.xyz>
2024-08-08 16:49:30 +0000

    Implement a blockhash fetcher as an AsyncIterator

--- utils/misc.mjs
+++ utils/misc.mjs
@@ -1,2 +1,7 @@
 export const sleep = async (duration) =>
   await new Promise((resolve) => setTimeout(resolve, duration));
+
+export const timeout = async (duration) =>
+  await new Promise((_, reject) => setTimeout(() => {
+    reject(new Error('Timeout'));
+  }, duration));
