bd1e9065b55aa2526de3d26d76f264d5ce2fb68f
Steven Luscher <steven.luscher@anza.xyz>
2024-10-06 06:36:34 +0000

    Sprinking dotenv configs where they need to go

--- utils/rpc.mjs
+++ utils/rpc.mjs
@@ -1,4 +1,7 @@
+import dotenv from 'dotenv';
 import { createSolanaRpc, createSolanaRpcSubscriptions_UNSTABLE } from "@solana/web3.js";
+
+dotenv.config();
 
 export const rpc = createSolanaRpc(
     process.env.RPC_ENDPOINT,
